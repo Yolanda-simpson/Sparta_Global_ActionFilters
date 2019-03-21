2fe6da0a93065a8e338379bb589d63b8da009222
yolanda <ysimpson@spartaglobal.com>
2019-03-21 09:24:52 +0000

    file added

---
+++ ActionFilters/ActionFilters/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace ActionFilters.Models
+{
+    public class Login
+    {
+        private int LoginID;
+        private string username;
+        private string password;
+
+        [DisplayName("LoginID")]
+        public int LoginID1 { get => LoginID; set => LoginID = value; }
+        [DisplayName("Username")]
+        public string Username { get => username; set => username = value; }
+        [DisplayName("Password")]
+        public string Password { get => password; set => password = value; }
+       
+    }
+}
