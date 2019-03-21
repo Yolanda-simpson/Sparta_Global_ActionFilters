3002bdef409d1d0d766b0aee130f7239d7a9130b
yolanda <ysimpson@spartaglobal.com>
2019-03-21 11:18:11 +0000

    moved controllers to correct place

--- ActionFilters/ActionFilters/Controllers/authentication.cs
+++ ActionFilters/ActionFilters/Controllers/authentication.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
 
-namespace ActionFilters.Controllers
-{
-    public class authentication
-    {
-    }
-}
+//namespace ActionFilters.Controllers
+//{
+//    public class authentication : ActionFilterAttribute, IAuthenticationFilter
+//    {
+//        void OnAuthentication(AuthenticationContext filterContext)
+//        {
+
+//        }
+
+//    }
+
+//     void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+//    {
+
+//    }
+   
+//}
--- ActionFilters/ActionFilters/Models/Login.cs
+++ ActionFilters/ActionFilters/Models/Login.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel;
 
+
 namespace ActionFilters.Models
 {
     public class Login
